6f3aa0047eb557d2c11e91588f1a38aabeb43e46
Sehar <saher.tariq027@gmail.com>
2026-05-06 20:51:49 +0500

    Fixed flake8 formatting

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return "Jenkins CI/CD Assignment Working!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
